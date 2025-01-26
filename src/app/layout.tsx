929711305a284b5bd68530a5158bc6c3802eba10
Mash <macbook@Mahamads-MacBook-Pro.local>
2025-01-26 18:13:31 +0000

    Fix favicon configuration and add web manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,10 +73,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="icon" href="/images/favicon.svg" type="image/svg+xml" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/images/favicon.svg" sizes="any" />
+        <link rel="mask-icon" href="/images/favicon.svg" color="#000000" />
         <link rel="apple-touch-icon" href="/images/favicon.svg" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
         <meta name="theme-color" content="#000000" />
         
